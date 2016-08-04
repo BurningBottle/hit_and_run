ae3683c967ea805c4a45ec30de9f04f05de554ff
JongHyuk Park <burnbottle@gmail.com>
2016-08-04 17:05:12 +0900

    * Add Lobby Scene ** Implement start server & join to server UI

---
+++ Assets/Script/LobbyUIManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class LobbyUIManager : MonoBehaviour
+{
+	public GameObject mainButtons;
+	public GameObject serverSideObjects;
+	public GameObject clientSideObjects;
+	public Text myIpText;
+	public InputField ipInputField;
+
+	public void OnClickStartServer()
+	{
+		mainButtons.SetActive(false);
+		serverSideObjects.SetActive(true);
+		myIpText.text = "IP : " + Network.player.ipAddress;
+    }
+
+	public void OnClickJoin()
+	{
+		mainButtons.SetActive(false);
+		clientSideObjects.SetActive(true);
+	}
+
+	public void OnClickConfirmJoin()
+	{
+		Debug.Log("Confirm to IP(" + ipInputField.text + ")");
+	}
+}
